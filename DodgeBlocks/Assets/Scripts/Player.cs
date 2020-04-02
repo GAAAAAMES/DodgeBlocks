fce86ea862beef4d860e3d6cb527a56121ab018b
Klaus33 <claudiup340@gmail.com>
2020-04-02 13:54:46 +0300

    Add player sprite

--- DodgeBlocks/Assets/Scripts/Player.cs
+++ DodgeBlocks/Assets/Scripts/Player.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public Sprite hitMaterial;
+    SpriteRenderer rend;
+
+    void Start(){	
+	rend = GetComponent<SpriteRenderer>();
+	rend.enabled = true;
+    }	
+
     private void OnCollisionEnter2D()
     {
+       rend.sprite = hitMaterial;
        FindObjectOfType<GameManager>().EndGame();
     }
 }
